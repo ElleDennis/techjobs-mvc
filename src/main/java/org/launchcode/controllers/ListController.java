e89879e876190cc924305c23bc7992f1abe718d2
Elle Dennis <elledennis@att.net>
2017-06-12 19:25:23 -0500

    Finished app code

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -17,8 +17,11 @@ import java.util.HashMap;
 public class ListController {
 
     static HashMap<String, String> columnChoices = new HashMap<>();
+    //This is a constructor. It allows for data to be initialized, populates columns with values?
+    //HashMap provides central collection of different list and search options
 
     public ListController () {
+        //This is a constructor. It allows for data to be initialized, populates columns with values
         columnChoices.put("core competency", "Skill");
         columnChoices.put("employer", "Employer");
         columnChoices.put("location", "Location");
@@ -28,7 +31,7 @@ public class ListController {
 
     @RequestMapping(value = "")
     public String list(Model model) {
-
+        //Displays the different types of lists for view
         model.addAttribute("columns", columnChoices);
 
         return "list";
@@ -36,14 +39,18 @@ public class ListController {
 
     @RequestMapping(value = "values")
     public String listColumnValues(Model model, @RequestParam String column) {
-
+        //Method uses query parameter passed in (column) to determine the values to fetch from JobData
+        //Displays actual data obtained from JobData
         if (column.equals("all")) {
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
+            //Fetches all job data. Passes data to list-jobs.html.
+
             model.addAttribute("title", "All Jobs");
             model.addAttribute("jobs", jobs);
             return "list-jobs";
         } else {
             ArrayList<String> items = JobData.findAll(column);
+            //Fetches only for requested column. Passes values to list-column.html
             model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
@@ -54,8 +61,11 @@ public class ListController {
 
     @RequestMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model,
-            @RequestParam String column, @RequestParam String value) {
-
+                                           @RequestParam String column, @RequestParam String value) {
+        //Method takes in 2 query parameters: column and value.
+        // Search is for specific value in a specific column & displays jobs that match.
+        //displays actual jobs matching specific value in a specific column
+        //User doesn't submit form. User clicks on link.
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
         model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,12 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by LaunchCode
@@ -19,9 +21,34 @@ public class SearchController {
     @RequestMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", ListController.columnChoices);
+
         return "search";
     }
+    // TODO #1 - Create handler to process search request and display results. DID IT!
+    //My code below...
 
-    // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    // request path: /results as seen in Spring Boot video
+    //ArrayList hashmap goes below
+    //If statement goes below
+    public String processSearchResult(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
 
+        if (searchType.equals("all")) {
+            ArrayList<HashMap<String, String>> jobs = JobData.findByValue(searchTerm);
+
+
+            model.addAttribute("title", "All Jobs");
+            model.addAttribute("jobs", jobs);
+            return "search";
+
+        } else {
+
+                ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+
+                model.addAttribute("title", "Jobs with " + searchType + ": " + searchTerm);
+                model.addAttribute("jobs", jobs);
+                model.addAttribute("columns", ListController.columnChoices);
+                return "search";
+        }
+    }
 }
